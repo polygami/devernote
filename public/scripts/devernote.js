c84a639f0b9118ed901ac62a096e6020598b078f
Liam Owen <liam@polygami.co.uk>
2018-02-23 12:53:01 +0100

    simplify convertToHtml() and remove unnecessary code

--- public/scripts/devernote.js
+++ public/scripts/devernote.js
@@ -1,39 +1,21 @@
 $('#sidebar-btn').on('click', function () {
 	$('.ui.sidebar').sidebar('toggle');
-  });
-  
-  $('.ui.accordion').accordion({ exclusive: false });
-  $('.ui.buttons .button').on('click', function() {
+});
+
+$('.ui.accordion').accordion({ exclusive: true });
+
+$('.ui.buttons .button').on('click', function() {
 	$(this).addClass('positive')
-		   .siblings()
-		   .removeClass('positive');
+  .siblings()
+  .removeClass('positive');
 	$('.treemenu').toggleClass('boxed');
-  });
-  
-  
-  $('.ui.accordion')
-	.accordion({
-	  exclusive: true
-	})
-  ;
+});
 
-  
+var converter = new showdown.Converter();
 
-window.onload = function() {
-    var converter = new showdown.Converter();
-    var markdown = document.getElementById('markdown');
-    var rendered = document.getElementById('rendered');   
-    var convertMarkdownToHtml = function(){
-        var markdownText = markdown.value;
-        html = converter.makeHtml(markdownText);
-        rendered.innerHTML = html;
-        // document.querySelectorAll('pre code').forEach(function(element) {
-        //   hljs.highlightBlock(element);
-        // });
-    };
-    
-    markdown.addEventListener('input', convertMarkdownToHtml);
-    convertMarkdownToHtml();
-
-
-};
+function convertToHtml(obj, otherObj) {
+  var rendered = document.getElementById(otherObj);
+  if (rendered) {
+    rendered.innerHTML = converter.makeHtml(obj.value);
+  }
+}
